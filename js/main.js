ca3d991329c4cef3fb8367433e570b1c757289c6
HHCHU <ded06031@snu.ac.kr>
2023-06-19 12:48:02 +0900

    add page4 layout

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const page1 = document.getElementById("page1");
 const page2 = document.getElementById("page2");
 const page3 = document.getElementById("page3");
-const pages = [page1, page2, page3];
+const pages = [page1, page2, page3, page4];
 let currentPage = "page1";
 let currentTitle = "";
 const nextPageBtn = document.getElementById("nextPageBtn");
@@ -67,6 +67,11 @@ const onClickNextPageBtn = (e) => {
       openPage(page3);
       currentPage = "page3";
     }
+  } else if (currentPage === "page3") {
+    closeAllPages();
+    currentPage = "page3";
+    openPage(page4);
+    createPage2();
   }
   console.log("To", currentPage);
 };
---
+++ js/pages/page4.js
@@ -0,0 +1,8 @@
+// 343540
+let canvasWidth;
+let canvasHeight;
+//DOM
+const p3MainCC = document.getElementById("p4MainCC");
+
+canvasWidth = p4MainCC.getBoundingClientRect().width;
+canvasHeight = p4MainCC.getBoundingClientRect().height;
